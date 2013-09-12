58b1927e6db255f2fe3fed182d04a45f29dcabc1
RIVIERE Gregory <gregory.riviere@laposte.net>
2013-09-12 11:49:08 +0200

    premier commit

---
+++ src/fr/treeptik/consommateurjms/MessageDB.java
@@ -0,0 +1,18 @@
+package fr.treeptik.consommateurjms;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+
+@MessageDriven(activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destination", propertyValue = "queue/TestQueue") })
+public class MessageDB implements MessageListener {
+
+	@Override
+	public void onMessage(Message message) {
+
+	}
+
+}
