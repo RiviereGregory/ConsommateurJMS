755bb2b3eabd1da424b44053a7b8341d44a30021
RIVIERE Gregory <gregory.riviere@laposte.net>
2013-09-12 14:40:44 +0200

    Nettoyage

--- src/fr/treeptik/consommateurjms/MessageDB.java
+++ src/fr/treeptik/consommateurjms/MessageDB.java
@@ -37,11 +37,11 @@ public class MessageDB implements MessageListener {
 
 		try {
 			jaxbContext = JAXBContext.newInstance("fr.treeptik.samplejms.xml");
-			// System.out.println("Dans try jaxb");
+
 			File fileEcriture = new File("client.xml");
 			if (!fileEcriture.exists()) {
 				try {
-					// System.out.println("Creation Fichier xml");
+
 					fileEcriture.createNewFile();
 				} catch (IOException e) {
 					e.printStackTrace();
@@ -57,22 +57,12 @@ public class MessageDB implements MessageListener {
 			// Lecture du fichier client
 			Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 			Client newClient = (Client) unmarshaller.unmarshal(new File("client.xml"));
+
 			System.out.println(newClient.getPersonne().size());
 
 		} catch (JAXBException | IOException | JMSException e) {
 			System.out.println("Dans catch jaxb");
 			e.printStackTrace();
 		}
-
-		// try {
-		// ObjectMessage objectMessage = (ObjectMessage) message;
-		// Produit produit = (Produit) objectMessage.getObject();
-		//
-		// System.out.println("Ref : " + produit.getRef());
-		// System.out.println("Desc : " + produit.getDescription());
-		// } catch (JMSException e) {
-		// e.printStackTrace();
-		// }
-
 	}
 }
